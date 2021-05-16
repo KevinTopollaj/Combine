0e6d4d86235443e8fa694d4f57083bf2f1d26ca8
Kevin Topollaj <kevintopollaj@gmail.com>
2021-05-16 20:16:37 +0200

    dataTaskPublisher

--- 01. Subscription.playground/Pages/subscribe on urlsession.xcplaygroundpage/Contents.swift
+++ 01. Subscription.playground/Pages/subscribe on urlsession.xcplaygroundpage/Contents.swift
@@ -7,13 +7,16 @@ import PlaygroundSupport
 PlaygroundPage.current.needsIndefiniteExecution = true
 
 let subscription = URLSession.shared.dataTaskPublisher(for: URL(string: "https://jsonplaceholder.typicode.com")!)
-   .map({ result in
+  .map({ result in
     print(Thread.current.isMainThread)
   })
-    .receive(on: DispatchQueue.main)
-    .sink(receiveCompletion: { _ in }, receiveValue: { value in
-        print(Thread.current.isMainThread)
-    })
+  // to set the upstream to main but it can not change it because dataTaskPublisher is set to run on the background.
+  .subscribe(on: DispatchQueue.main)
+  .receive(on: DispatchQueue.main)
+  .sink(receiveCompletion: { _ in },
+        receiveValue: { value in
+    print(Thread.current.isMainThread)
+  })
 
 
 //: [Next](@next)
