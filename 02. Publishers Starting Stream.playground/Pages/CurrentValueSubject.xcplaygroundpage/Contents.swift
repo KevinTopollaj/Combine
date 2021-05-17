4692668c59f03cb8be1b0ecf7242ee91437b5abc
Kevin Topollaj <kevintopollaj@gmail.com>
2021-05-17 23:52:18 +0200

    working with CurrentValueSubject

--- 02. Publishers Starting Stream.playground/Pages/CurrentValueSubject.xcplaygroundpage/Contents.swift
+++ 02. Publishers Starting Stream.playground/Pages/CurrentValueSubject.xcplaygroundpage/Contents.swift
@@ -1,9 +1,11 @@
 //: [Previous](@previous)
 
-//Subject - Publisher that you can continously send new values down
+//Subject - Publisher that you can continously send new values down stream.
 
 //CurrentValueSubject
 //used like a var with a Publisher stream attached
+
+// CurrentValueSubject must have a starting value.
 
 import Foundation
 import Combine
@@ -16,21 +18,30 @@ struct User {
     var name: String
 }
 
-let currentUserId = CurrentValueSubject<Int     , Never>(1000)
-let userNames     = CurrentValueSubject<[String], Never>(["Bob", "Susan", "Luise"])
+let currentUserId = CurrentValueSubject<Int, Never>(1000)
+let userNames = CurrentValueSubject<[String], Never>(["Bob", "Susan", "Luise"])
 
-let currentUser   = CurrentValueSubject<User    , Never>(User(id: 1, name: "Bob"))
-
+let currentUser = CurrentValueSubject<User, Never>(User(id: 1, name: "Bob"))
 
 
 // get the value for currentUserID
+print("Current User Id: \(currentUserId.value)")
 
 // subscribe to Subject
+let subscription = currentUserId.sink {
+  print("completion: \($0)")
+} receiveValue: { value in
+  print("recived value: \(value)")
+}
 
 // passing down new values with Subject
+currentUserId.send(1)
+currentUserId.send(2)
 
 // sending completion finished with Subject
+currentUserId.send(completion: .finished)
 
-
+// after finishing it will not recive any more values
+currentUserId.send(12)
 
 //: [Next](@next)
