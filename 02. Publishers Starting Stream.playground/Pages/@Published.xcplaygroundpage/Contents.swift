c6eea833b71faccda9b95e99d764ec8573f38de2
Kevin Topollaj <kevintopollaj@gmail.com>
2021-05-20 21:30:51 +0200

    working with @Published

--- 02. Publishers Starting Stream.playground/Pages/@Published.xcplaygroundpage/Contents.swift
+++ 02. Publishers Starting Stream.playground/Pages/@Published.xcplaygroundpage/Contents.swift
@@ -3,8 +3,8 @@
 /*
  @Published property wrapper
  adds a Publisher to a property
-
-- use `@Published` for class propeties not structures.
+ 
+ - use `@Published` for class propeties not structures.
  
  */
 
@@ -16,25 +16,35 @@ import PlaygroundSupport
 PlaygroundPage.current.needsIndefiniteExecution = true
 
 class ViewModel {
+  
+  // use @Published to create publisher
+  @Published var userNames: [String] = ["Bill"]
+  
+  //    let userNames = CurrentValueSubject<[String], Never>(["Bill"])
+  let newUserNameEntered = PassthroughSubject<String, Never>()
+  
+  var subscriptions = Set<AnyCancellable>()
+  
+  init() {
+    // we can get the Publisher and Subscribe to it
+    $userNames.sink {
+      print("recive value \($0)")
+    }.store(in: &subscriptions)
     
-    // use @Published to create publisher
-    let userNames = CurrentValueSubject<[String], Never>(["Bill"])
-    let newUserNameEntered = PassthroughSubject<String, Never>()
-    
-    var subscriptions = Set<AnyCancellable>()
-    
-    init() {
-        
-        
-    }
+    newUserNameEntered.sink { [unowned self] value in
+      self.userNames.append(value)
+    }.store(in: &subscriptions)
+  }
 }
 
 let viewModel = ViewModel()
 
 
 // add new user name "Susan"
+viewModel.newUserNameEntered.send("Suzan")
 
 // add new user name "Bob"
+viewModel.newUserNameEntered.send("Bob")
 
 //  Documentation: When the property changes, publishing occurs in the property's `willSet` block, meaning subscribers receive the new value before it's actually set on the property.
 
